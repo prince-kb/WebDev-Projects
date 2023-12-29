4237424848a1da5f21f2909219d6cc4e71fb3809
Prince <princejaj67@gmail.com>
2023-12-30 00:35:22 +0530

    Completed drag and drop project

--- 13_DragAndDrop/script.js
+++ 13_DragAndDrop/script.js
@@ -1,11 +1,18 @@
-
-const boxes=document.getElementsByClassName("box"),
-    image=document.getElementsByClassName("image")
-console.log(boxes,image);
-
-boxes.forEach(b=>{
-    b.addEventListener("dragover",e => {
+    const box1=document.querySelectorAll(".box");
+    const image = document.querySelector('.image')
+    console.log(box1)
+box1.forEach(b=>{
+    b.addEventListener("dragover",(e) => {
         e.preventDefault();
-        console.log("Dragging")
+        // console.log("Dragging")
+        b.classList.add("hovered");
     })
+    b.addEventListener("dragleave",()=>{
+        b.classList.remove("hovered");
+    })
+    b.addEventListener("drop",()=>{
+        b.appendChild(image);
+        b.classList.remove("hovered");
+    })
+    
 })
